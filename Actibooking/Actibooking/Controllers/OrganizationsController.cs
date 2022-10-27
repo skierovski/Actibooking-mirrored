8a12f2790af80add1db556777d6cf53b2a8eabfe
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-10-27 10:56:37 +0200

    Creating CRUD Controllers Base

---
+++ Actibooking/Actibooking/Controllers/OrganizationsController.cs
@@ -0,0 +1,39 @@
+﻿using Actibooking.Data;
+using Actibooking.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actibooking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationsController : ControllerBase
+    {
+
+        [HttpGet("get-all-organizations")]
+        public async Task<bool> GetAll()
+        {
+            return true;
+        }
+
+        [HttpPost("create-organization")]
+        public async Task<bool>CreateOrganization(Organization organization)
+        {
+            return true;
+        }
+
+        [HttpDelete]
+        [Route("delete-organization/{id}")]
+        public async Task<bool> DeleteOrganization(int organizationId)
+        {
+            return true;
+        }
+
+        [HttpPut]
+        [Route("update-organization/{id}")]
+        public async Task<bool> UpdateOrganization(int organizationId)
+        {
+            return true;
+        }
+    }
+}
