bb438b8aa533126fd551fab26ab9e8de6c1bf2bc
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-11-02 11:57:26 +0100

    Form completed fetches with api and create user

--- actibooking-react/src/components/Authentication/SignUp.js
+++ actibooking-react/src/components/Authentication/SignUp.js
@@ -1,15 +1,35 @@
 import React from "react";
 import {useForm} from "react-hook-form";
+import { json } from "react-router-dom";
 import PostDataHandler from "../../data/PostDataHandler";
 export default function SignUp() {
     const {register, handleSubmit, errors} = useForm();
-    const onSubmit = async (values) => {
-        await PostDataHandler("https://localhost:7127/api/Account/register", values);
+    const receiveResponseStatus = status => {
+        if (status !== true) {
+            console.log("Good")}
+        else console.log("Bad");
+    }
+    const onSubmit = (values) => {
+        const UserData = {
+            'email': values.email,
+            'password': values.password,
+            'firstname': values.firstname,
+            'lastname': values.lastname,
+            'phoneNumber': values.phoneNumber,
+            "roles": [
+                "User"
+              ]
+        }
+        console.log(UserData)    
+        PostDataHandler('https://localhost:7127/api/Account/register', UserData, receiveResponseStatus)
+
     };
+
+
     return (
         <div className="sign-up-form">
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input  type="text" required placeholder="FirstName" name="firstname" {...register('firstName')}/>
+            <input  type="text" required placeholder="FirstName" name="firstname" {...register('firstname')}/>
             <input type="text" required placeholder="LastName" name="lastname" {...register('lastname')}/>
             <input type="text" required placeholder="Email" name="email" {...register('email')}/>
             <input type="text" required placeholder="Password" name="password" {...register('password')}/>
