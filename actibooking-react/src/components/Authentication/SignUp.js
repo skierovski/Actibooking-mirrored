fc4b36844d6b555df3e2ce72b85b028b5ef6021e
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-11-02 08:39:25 +0100

    Input are prepared

--- actibooking-react/src/components/Authentication/SignUp.js
+++ actibooking-react/src/components/Authentication/SignUp.js
@@ -1,19 +1,29 @@
 import React from "react";
 import {useForm} from "react-hook-form";
-
+import PostDataHandler from "../../data/PostDataHandler";
 export default function SignUp() {
     const {register, handleSubmit, errors} = useForm();
-    const onSubmit = (data) => {
-
-    }
-    return(
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="FirstName" name="FirstName" ref={register}/>
-            <input type="text" placeholder="FirstName" name="FirstName" ref={register}/>
-            <input type="text" placeholder="FirstName" name="FirstName" ref={register}/>
-            <input type="text" placeholder="FirstName" name="FirstName" ref={register}/>
-        </form>
+    const onSubmit = async (values) => {
+        await PostDataHandler("https://localhost:7127/api/Account/register", values);
+    };
+    return (
+        <div className="sign-up-form">
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input  type="text" required placeholder="FirstName" name="firstname" {...register('firstName')}/>
+            <input type="text" required placeholder="LastName" name="lastname" {...register('lastname')}/>
+            <input type="text" required placeholder="Email" name="email" {...register('email')}/>
+            <input type="text" required placeholder="Password" name="password" {...register('password')}/>
+            <input type="text" required placeholder="PhoneNumber" name="phoneNumber" {...register('phoneNumber')}/>
+            <input type="submit" />
+       </form>
+        </div>
     );
-
-
 }
+
+// "email": "user@example.com",
+// "password": "string",
+// "firstName": "string",
+// "lastName": "string",
+// "phoneNumber": "string",
+// "roles": [
+//   "string"
