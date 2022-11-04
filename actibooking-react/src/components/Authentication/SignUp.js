5c53e5a939dba4eeeed34a12377975b115bb1851
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-11-04 15:35:41 +0100

    Validation SignUp

--- actibooking-react/src/components/Authentication/SignUp.js
+++ actibooking-react/src/components/Authentication/SignUp.js
@@ -1,21 +1,23 @@
 import React from "react";
 import {useForm} from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import PostDataHandler from "../../data/PostDataHandler";
 import './SignUp.css'
 export default function SignUp() {
     const {register, handleSubmit, errors} = useForm();
+    const[isValidInput, setIsValidInput] = useState(true);
     let navigate = useNavigate();
     const changeRoute = () =>{
         let path = `/`;
         navigate(path);
     }
     const receiveResponseStatus = status => {
-        if (status !== true) {
+        if (status === true) {
             console.log("Good");
             changeRoute();
         }
-        else console.log("Bad");
+        else setIsValidInput(false);
     }
     const onSubmit = (values) => {
         const UserData = {
@@ -47,7 +49,7 @@ export default function SignUp() {
             </div>
             <div className="row mt-3">
             <div className="col">
-            <input className="form-control" type="email" required placeholder="Email" name="email" {...register('email')}/>
+            <input className="form-control" style={{borderColor: isValidInput? "none":"red"}} type="email" required placeholder="Email" name="email" {...register('email')}/>
             </div>
             <div className="col">
             <input className="form-control" type="password" placeholder="Password" name="password" {...register('password')} pattern="(?=.*\d)(?=.*[\W_]).{5,}" required/>
