1213e148100278bca61639766e3e87da71282857
AdrianRymaszewski <arymasz87@gmail.com>
2022-11-14 19:41:17 +0100

    Add Test

---
+++ Actibooking/Actibooking.Test/AccountControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using Actibooking.Controllers;
+using Actibooking.Models;
+using Actibooking.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actibooking.Test
+{
+    public class AccountControllerTest
+    {
+        [Fact]
+        public async void TryValidateNewUser()
+        {
+
+            var userMenagerMock = new Mock<UserManager<ActiBookingUser>>();
+            var iMapperMock = new Mock<IMapper>();
+            var iLoggerAccountControllerMock = new Mock<ILogger<AccountController>>();
+            var iAuthManagerMock = new Mock<IAuthManager>();
+            var accountController = new AccountController(userMenagerMock.Object, iMapperMock.Object, iLoggerAccountControllerMock.Object, iAuthManagerMock.Object);
+
+            var newUserDTO = new UserDTO();
+            newUserDTO.Email = "ar12@actibooking.pl";
+            newUserDTO.Password = "P@ssword123";
+            newUserDTO.FirstName = "Adrian";
+            newUserDTO.LastName = "Testowe";
+            newUserDTO.Roles.Add("User");
+
+            var newUser = await accountController.Register(newUserDTO);
+            Assert.IsType<OkObjectResult>(newUser);
+        }
+
+
+    }
+}
---
+++ Actibooking/Actibooking.Test/CoursesTagControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using Actibooking.Controllers;
+using Actibooking.Data.Repository;
+using Actibooking.Models;
+using Actibooking.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actibooking.Test
+{
+    public class CoursesTagControllerTest
+    {
+        [Fact]
+        public async void CreateNewCourseTag_TEST()
+        {
+            var iUnitOfWork = new Mock<IUnitOfWork>();
+            var iLoggerCoursesTagControllerMock = new Mock<ILogger<CoursesTagController>>();
+            var coursesTagController = new CoursesTagController(iUnitOfWork.Object,  iLoggerCoursesTagControllerMock.Object);
+
+            var Tag = new CourseTag();
+            Tag.Description = "Testowy";
+            Tag.Name = "Testowy";
+            Tag.Id = 10;
+            Tag.Courses = null;
+
+            var newCoursesTag = await coursesTagController.CreateCourseTag(Tag);
+            Assert.IsType<ObjectResult>(newCoursesTag);
+        }
+
+
+        [Fact]
+        public async void IsExistCourseTag_TEST()
+        {
+            var iUnitOfWork = new Mock<IUnitOfWork>();
+            var iLoggerCoursesTagControllerMock = new Mock<ILogger<CoursesTagController>>();
+            var coursesTagController = new CoursesTagController(iUnitOfWork.Object, iLoggerCoursesTagControllerMock.Object);
+
+            var Tag = new CourseTag();
+            Tag.Description = "Kid";
+
+
+            var CoursesTags = await coursesTagController.GetAll();
+
+        }
+
+    }
+}
