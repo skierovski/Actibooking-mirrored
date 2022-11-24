86cefa2823e460ec91bf2811fe08138ca51facb1
AdrianRymaszewski <arymasz87@gmail.com>
2022-11-24 22:06:52 +0100

    Add Category Slider

--- actibooking_react_app/src/App.js
+++ actibooking_react_app/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import CategorySlideCard from "./CategorySlideCard/CategorySlideCard";
 
 function App() {
   return (
     <div className="App">
+      <CategorySlideCard />
     </div>
   );
 }
---
+++ actibooking_react_app/src/CategorySlideCard/Data.js
@@ -0,0 +1,54 @@
+export const CategoryData = [
+  {
+    CategoryId: 1,
+    CategoryName: "Football School",
+    ImageURL: "CategoryImg/1.jpeg",
+  },
+  {
+    CategoryId: 2,
+    CategoryName: "Basketball School",
+    ImageURL: "./CategoryImg/2.jpeg",
+  },
+  {
+    CategoryId: 3,
+    CategoryName: "Gym",
+    ImageURL: "./CategoryImg/3.jpeg",
+  },
+  {
+    CategoryId: 4,
+    CategoryName: "Dance School Kids",
+    ImageURL: "./CategoryImg/4.jpeg",
+  },
+  {
+    CategoryId: 5,
+    CategoryName: "Swimming School",
+    ImageURL: "./CategoryImg/5.jpeg",
+  },
+  {
+    CategoryId: 6,
+    CategoryName: "Massage",
+    ImageURL: "./CategoryImg/6.jpg",
+  },
+  {
+    CategoryId: 7,
+    CategoryName: "Personal Trainer",
+    ImageURL: "./CategoryImg/7.jpeg",
+  },
+  {
+    CategoryId: 8,
+    CategoryName: "Dance School Adult",
+    ImageURL: "./CategoryImg/8.jpeg",
+  },
+  {
+    CategoryId: 9,
+    CategoryName: "Rehabilitation",
+    ImageURL: "./CategoryImg/9.jpeg",
+  },
+  {
+    CategoryId: 10,
+    CategoryName: "Gymnastics",
+    ImageURL: "./CategoryImg/10.jpeg",
+  },
+];
+
+export default CategoryData;
