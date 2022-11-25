5a7a2451dc76ec9beffae1a3ca061b70b68b4a27
AdrianRymaszewski <arymasz87@gmail.com>
2022-11-25 14:34:32 +0100

    Add style to Footer

--- actibooking_react_app/src/Components/Footer/Footer.js
+++ actibooking_react_app/src/Components/Footer/Footer.js
@@ -1,33 +1,45 @@
-import styles from './Footer.module.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faInstagram, faTwitter, faFacebook, faGithub} from '@fortawesome/free-brands-svg-icons'
+import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faTwitter,
+  faFacebook,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-    return (
-        <div className={styles.footer}>
-            <div className={styles.left}>
-                <div className={styles.logo}>
-                <p>Actibooking</p>
-                </div>
-                <p>© 2022 Actibooking Inc. All rights reserved.</p>
-            </div>
-            <div className={styles.link}>
-                <p>About us</p>
-                <p>FAQ</p>
-                <p>Privacy Policy</p>
-                <p>How To</p>
-                </div>
-            <div className={styles.privacy}>
-                <div className={styles.icons}>
-                <FontAwesomeIcon icon={faInstagram} className={styles.social}/>
-                <FontAwesomeIcon icon={faTwitter} className={styles.social}/>
-                <FontAwesomeIcon icon={faFacebook} className={styles.social}/>
-                <FontAwesomeIcon icon={faGithub} className={styles.social}/>
-                </div>
-            </div>
-
+  return (
+    <div className={styles.footer}>
+      <div className={styles.left}>
+        <p className={styles.logo}>Actibooking</p>
+        <p className={styles.copyright}>
+          © Copyright 2022 Team-APA. All rights reserved.
+        </p>
+      </div>
+      <div className={styles.link}>
+        <a href="link/" className={styles.links}>
+          About us
+        </a>
+        <a href="link/" className={styles.links}>
+          FAQ
+        </a>
+        <a href="link/" className={styles.links}>
+          Privacy Policy
+        </a>
+        <a href="link/" className={styles.links}>
+          How To
+        </a>
+      </div>
+      <div className={styles.privacy}>
+        <div className={styles.icons}>
+          <FontAwesomeIcon icon={faInstagram} className={styles.social} />
+          <FontAwesomeIcon icon={faTwitter} className={styles.social} />
+          <FontAwesomeIcon icon={faFacebook} className={styles.social} />
+          <FontAwesomeIcon icon={faGithub} className={styles.social} />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
