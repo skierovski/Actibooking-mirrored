81762a07b0ccbb2b319b5a622efd93b1694c0849
AdrianRymaszewski <arymasz87@gmail.com>
2022-11-30 20:19:10 +0100

    fix

--- actibooking_react_app/src/Components/Organization/MostPupularOrganization/MostPopularOrganization.js
+++ actibooking_react_app/src/Components/Organization/MostPupularOrganization/MostPopularOrganization.js
@@ -1,46 +1,9 @@
-/* import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import MostPopularOrganizationData from "../../../Data/MostPopularOrganizationData";
-import styles from "./MostPopularOrganization.module.css"
-import SliderCard from "../OrganizationsCard/SliderCard";
-import NextArrow from "./Arrows/NextArrow";
-import PrevArrow from "./Arrows/PrevArrow";
-
-
-export default function MostPopularOrganization() {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "10px",
-    slidesToShow: 4,
-    speed: 500,
-    dots: true,
-    nextArrow: <NextArrow/>,
-    prevArrow: <PrevArrow/>
-  };
-
-  return (
-    <div className={styles.OrganizationsSlider}>
-      <p>Our Recomended Organizations</p>
-        <div className={styles.slaiderbox}>
-        <Slider {...settings}>
-          {MostPopularOrganizationData.map((o) => (
-          <SliderCard key={o.id} logoUrl={o.logoUrl} name={o.name} adress={o.adresses[0]}/>
-        ))}
-        </Slider>
-        </div>
-      </div>
-  );
-} */
 
 import React from "react";
-// Import Swiper React components
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import Organizations from "../../../Data/MostPopularOrganizationData";
-// Import Swiper styles
+
 import "swiper/css";
 import OrganizationCard from "./OrganizationCard";
 import SectionTitle from "../../DefaultModels/Titles/SectionTitle";
@@ -48,7 +11,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./MostPopularOrganization.css";
 
-// import required modules
+
 import { Navigation } from "swiper";
 export default function MostPopularOrganization() {
   return (
