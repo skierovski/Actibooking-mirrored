5b7cb6fc65320b3380611ff498ceaaff81d71b3c
AdrianRymaszewski <arymasz87@gmail.com>
2022-12-02 20:34:39 +0100

    Fic SingleOrganization

--- actibooking_react_app/src/Components/Organization/SingleOrganizationPage/SingleOrganizationContainer.js
+++ actibooking_react_app/src/Components/Organization/SingleOrganizationPage/SingleOrganizationContainer.js
@@ -1,23 +1,27 @@
-import SectionTitle from "../../DefaultModels/Titles/SectionTitle";
 import styles from "./SingleOrganizationContainer.module.css";
 
-const SingleOrganizationContainer = params =>{
-    const organization = params.organization;
-    return(
-        <div className={styles.singleOrganizationContainer}>
-            <div className={styles.imageDataContainer}>
-                <div className={styles.imageContainer}><img alt="organization image" className={styles.image} src={organization.logoUrl}/></div>
-            </div>
-            <div className={styles.mainInfoContainer}>
-                    <div className={styles.nameContainer}>{organization.name}</div>
-                    <div className={styles.adressContainer}>{organization.adresses}</div>
-            </div>
-            <div className={styles.descriptionContainer}>
-            <div className={styles.descriptionSection}>
-                <div className={styles.description}>{organization.description}</div>
-            </div>
-            </div>
+const SingleOrganizationContainer = (params) => {
+  const organization = params.organization;
+  return (
+    <div className={styles.singleOrganizationContainer}>
+      <div className={styles.mainDataContainer}>
+        <div className={styles.imageContainer}>
+          <img
+            alt="organization image"
+            className={styles.image}
+            src={organization.logoUrl}
+          />
         </div>
-    )
-}
+        <div className={styles.mainInfoContainer}>
+          <div className={styles.nameContainer}>{organization.name}</div>
+          <div className={styles.adressContainer}>{organization.adresses}</div>
+        </div>
+      </div>
+      <div className={styles.descriptionContainer}>
+        {organization.description}
+      </div>
+    </div>
+  );
+};
+
 export default SingleOrganizationContainer;
