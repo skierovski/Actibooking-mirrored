5651fde502df67829f6922b742868331ef50e9a8
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-12-08 10:33:31 +0100

    some changes

--- actibooking_react_app/src/Components/Authorization/GoogleAthorization/GoogleLogInPage.js
+++ actibooking_react_app/src/Components/Authorization/GoogleAthorization/GoogleLogInPage.js
@@ -6,8 +6,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 function GoogleLogInPage() {
     const clientId = "337470745858-36e0ar5ddn0csbinl1ore0qor37t6imn.apps.googleusercontent.com";
     const responseGoogle = (response) => {
+      const token = response.credential;
         console.log(response);
-        const userObject = jwt_decode(response.credential);
+        const userObject = jwt_decode(token);
         console.log(userObject);
         localStorage.setItem('user', JSON.stringify(userObject));
         console.log(localStorage["user"])
@@ -16,9 +17,7 @@ function GoogleLogInPage() {
     return(
         <GoogleOAuthProvider clientId={clientId}>
         <GoogleLogin
-            onSuccess={credentialResponse => {
-                console.log(credentialResponse);
-              }}
+            onSuccess={responseGoogle}
             onFailure={responseGoogle}
             useOneTap={true}
             text= "signup_with"
