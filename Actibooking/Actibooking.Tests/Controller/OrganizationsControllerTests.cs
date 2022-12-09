4429ec1a28502f0992c3fb6a98799ba6e9b56a5f
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-12-09 19:50:34 +0100

    Create Project for testing

---
+++ Actibooking/Actibooking.Tests/Controller/OrganizationsControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using FakeItEasy;
+using Actibooking.Models;
+
+namespace Actibooking.Tests.Controller
+{
+    public class OrganizationsControllerTests
+    {
+        private IUnitOfWork _organizationManager;
+
+        public OrganizationsControllerTests()
+        {
+            _organizationManager = A.Fake<IUnitOfWork>();
+        }
+    }
+}
