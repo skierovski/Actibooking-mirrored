a9e49cd4165a6d729dc00ac0f08b501117e40fd9
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-12-11 22:20:17 +0100

    Implementing all tests for organization controller and manager

--- Actibooking/Actibooking.Tests/Manager/OrganiazationManagerTests.cs
+++ Actibooking/Actibooking.Tests/Manager/OrganiazationManagerTests.cs
@@ -54,5 +54,69 @@ namespace Actibooking.Tests.Service
             //Assert
             result.Exception.InnerException.Message.Equals($"Organization ({organizationList}) was not found");
         }
+
+        [Fact]
+        public void OrganizationManager_FindOrganization_ReturnOrganization()
+        {
+            //Arange
+            var organization = A.Fake<Organization>();
+            A.CallTo(() => _uow.OrganizationRepo.GetByIdAsync(organization.Id)).Returns(organization);
+            var manager = new OrganizationManager(_uow, _mapper);
+
+            //Act
+            var result = manager.FindOrganzation(organization.Id);
+
+            //Assert
+            result.Result.Equals(organization);
+        }
+        [Fact]
+        public void OrganizationManager_FindOrganization_ReturnExeption()
+        {
+            //Arange
+            var organization = A.Fake<Organization>();
+            A.CallTo(() => _uow.OrganizationRepo.GetByIdAsync(organization.Id)).Returns((Organization) null);
+            var manager = new OrganizationManager(_uow, _mapper);
+            //Act
+            var result = manager.FindOrganzation(organization.Id);
+
+            //Assert
+            result.Exception.InnerException.Message.Equals($"Organization ({organization.Id}) was not found");
+        }
+
+        [Fact]
+        public void OrgazationManager_MapOrganization_ReturnTrue()
+        {
+            //Arange
+            var newOrganizationDTO = A.Fake<NewOrganizationDTO>();
+            var organization = A.Fake<Organization>();
+            A.CallTo(() => _mapper.Map<Organization>(newOrganizationDTO)).Returns(organization);
+            A.CallTo(() => _uow.OrganizationRepo.InsertAsync(organization)).Returns(Task.CompletedTask);
+            A.CallTo(() => _uow.SaveChangesAsync()).Returns(Task.CompletedTask);
+            var manager = new OrganizationManager(_uow, _mapper);
+
+            //Act
+
+            var result = manager.MapOrganization(newOrganizationDTO);
+
+            //Assert
+            result.Result.Equals(true);
+        }
+
+        [Fact]
+        public void OrganizationManager_ConvertOrganization_ReturnTask()
+        {
+            var updateOrganizationDTO = A.Fake<UpdateOrganizationDTO>();
+            var organization = A.Fake<Organization>();
+            A.CallTo(() => _uow.OrganizationRepo.GetByIdAsync(updateOrganizationDTO.Id)).Returns(organization);
+            A.CallTo(() => _uow.OrganizationRepo.Update(organization)).DoesNothing();
+            A.CallTo(() => _uow.SaveChangesAsync()).DoesNothing();
+            var manager = new OrganizationManager(_uow, _mapper);
+
+            //Act
+            var result = manager.ConvertOrganization(updateOrganizationDTO);
+
+            //Assert
+           result.IsCompletedSuccessfully.Equals(true);
+        }
     }
 }
