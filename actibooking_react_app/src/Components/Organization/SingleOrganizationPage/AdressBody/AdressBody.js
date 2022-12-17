1a304d4d169301a7bf6bc5b34b80e6b42c9243f4
AdrianRymaszewski <arymasz87@gmail.com>
2022-12-17 21:45:00 +0100

    Add signle PageOrganization fatch to api

--- actibooking_react_app/src/Components/Organization/SingleOrganizationPage/AdressBody/AdressBody.js
+++ actibooking_react_app/src/Components/Organization/SingleOrganizationPage/AdressBody/AdressBody.js
@@ -1,13 +1,25 @@
-import styles from './AdressBody.module.css'
+import styles from "./AdressBody.module.css";
 
 const AdressBody = (props) => {
-    const organizationAdress = props.organizationAdress;
-    //Google map to add
-        return (
-            <div className={styles.adressContainer}>
-                {organizationAdress}
-             </div>
-        )
-}
+  const organizationAdress = props.organizationAdress;
+  console.log("Addres");
+  console.log(props);
+  //Google map to add
+  return (
+    <div className={styles.adressContainer}>
+      <p className={styles.address}>
+        <span className={styles.address}>
+          Kraj: {organizationAdress.country}
+        </span>
+        <span className={styles.address}>
+          Miasto: {organizationAdress.zipcode} {organizationAdress.city}
+        </span>
+        <span className={styles.address}>
+          Ulica: {organizationAdress.street} {organizationAdress.streetNumber}
+        </span>
+      </p>
+    </div>
+  );
+};
 
 export default AdressBody;
