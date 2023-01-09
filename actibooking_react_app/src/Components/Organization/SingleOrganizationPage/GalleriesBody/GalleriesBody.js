1b8491e09cb40bd9c120ff9a1e8999e045807838
AdrianRymaszewski <arymasz87@gmail.com>
2023-01-09 21:27:14 +0100

    Add new favico

--- actibooking_react_app/src/Components/Organization/SingleOrganizationPage/GalleriesBody/GalleriesBody.js
+++ actibooking_react_app/src/Components/Organization/SingleOrganizationPage/GalleriesBody/GalleriesBody.js
@@ -1,34 +1,41 @@
-import styles from './GalleriesBody.module.css'
-import React, {  useContext, useEffect, useRef, useState } from "react";
+import styles from "./GalleriesBody.module.css";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { storage } from "../../../../firebase";
 import {
-    ref,
-    uploadBytes,
-    getDownloadURL,
-    listAll,
-    list,}
-   from "firebase/storage";
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
 
 const GalleriesBody = (props) => {
-    const [imageUrls, setImageUrls] = useState([]);
-    const imagesListRef = ref(storage, `galery/${props.id}/`);
-    useEffect(() => {
-      listAll(imagesListRef).then((response) => {
-        response.items.forEach((item) => {
-          getDownloadURL(item).then((url) => {
-            setImageUrls((prev) => [...prev, url]);
-          });
+  const [imageUrls, setImageUrls] = useState([]);
+  const imagesListRef = ref(storage, `galery/${props.id}/`);
+  const image = []
+  const widths = [ 500, 1000, 1600 ]
+  const ratios = [ 2.2, 4, 6, 8 ]
+  imageUrls.forEach((url)=>{
+    image.push({src: url,aspect_ratio: 16/9,})
+  })
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
         });
       });
-    }, []);
-        return (
-            <div className={styles.trainersContainer}>
-                {/* {organizationTrainers.map(t => <div>{t.name}</div>)} */}
-                {imageUrls.map((url) => {
+    });
+  }, []);
+  return (
+    <div className={styles.trainersContainer}>
+      {/* {organizationTrainers.map(t => <div>{t.name}</div>)} */}
+{/*       <Gallery {...{image, widths, ratios}} /> */}
+{/*       {imageUrls.map((url) => {
         return <img className={styles.NewImage} src={url} />;
-      })}
-             </div>
-        )
-}
+      })} */}
+    </div>
+  );
+};
 
 export default GalleriesBody;
