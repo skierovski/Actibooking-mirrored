d6c1b95d6735a0dbad413ee3add4debe99d245c2
skierovski <98186890+skierovski@users.noreply.github.com>
2023-01-22 22:11:59 +0100

    Delete actibooking-react directory

--- actibooking-react/src/components/CardWrappers/BaseWrapper.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./BaseWrapper.css";
-
-const BaseWrapper = props => {
-    const classes = "card " + props.className;
-    return <div className={classes}>{props.children}</div>;
-}
-export default BaseWrapper;
