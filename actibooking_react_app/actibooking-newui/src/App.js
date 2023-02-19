020758bebc7216eaea87a96091c5a9e78e23d631
Patryk Skierkowski <salon.skierkowski@gmail.com>
2023-02-19 23:24:37 +0100

    Creating layount and basic presentation MUI

--- actibooking_react_app/actibooking-newui/src/App.js
+++ actibooking_react_app/actibooking-newui/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {Grid} from '@mui/material'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='Container'>
+        <Grid container>
+          <Grid item xs={8} sx={{display: 'flex', justifyContent: 'flex-start'}}>Logo</Grid>
+          <Grid items xs={2} sx={{display: 'flex', justifyContent: 'flex-end'}}>Przycisk</Grid>
+          <Grid items xs={2} sx={{display: 'flex', justifyContent: 'flex-end'}}>Przycisk</Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={6}>test</Grid>
+          <Grid items xs={6}>test</Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={4}>test</Grid>
+          <Grid items xs={8}>test</Grid>
+        </Grid>
+        
+      </div>
   );
 }
 
