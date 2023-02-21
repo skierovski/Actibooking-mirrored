1e47e27f83fb4e282d49ecd896a87613f4cbcade
Patryk Skierkowski <salon.skierkowski@gmail.com>
2023-02-21 21:19:33 +0100

    fix

--- actibooking_react_app/actibooking-newui/src/Components/Default/Modal/Modal.js
+++ actibooking_react_app/actibooking-newui/src/Components/Default/Modal/Modal.js
@@ -8,9 +8,9 @@ export default function Modal({open, onClose, children}) {
   return ReactDom.createPortal(
     <>
     <div className='modal'>
-    <Button animation='none' text='X' style={{position: 'absolute', fontSize: '1.3rem', border: 'none', borderRadius: '10px', backgroundColor: 'transparent', color: 'white', top: '2%', right: '2%'}} function={onClose}/>
         <div className='modal-content'>
             {children}
+            <Button animation='none' text='X' style={{position: 'absolute', fontSize: '1.3rem', border: 'none', borderRadius: '10px', backgroundColor: 'transparent', color: 'white', top: '2%', right: '2%'}} function={onClose}/>
         </div>
     </div>
     </>,
