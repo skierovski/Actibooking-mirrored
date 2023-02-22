9b8f9bebf6060eec7eae6201400f859c08c447dd
Patryk Skierkowski <salon.skierkowski@gmail.com>
2023-02-22 13:15:15 +0100

    implementing log in form

--- actibooking_react_app/actibooking-newui/src/App.js
+++ actibooking_react_app/actibooking-newui/src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
+import { useState, React, useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Components/Pages/Home/Home';
+import ResponsiveContext from './Context/ResponsiveContext';
+
 
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth)
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth)
+    
+}
+
+    window.addEventListener('resize', handleResize)
+  })
+
   return (
       <div className='Container'>
+        <ResponsiveContext.Provider value={{screenWidth: width}}>
         <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}>
         </Route>
       </Routes>
     </BrowserRouter>
+    </ResponsiveContext.Provider >
         
       </div>
   );
--- actibooking_react_app/actibooking-newui/src/Components/Default/Modal/Modal.js
+++ actibooking_react_app/actibooking-newui/src/Components/Default/Modal/Modal.js
@@ -1,16 +1,21 @@
 import Button from '../Button/Button'
-import React from 'react'
+import React, { useContext } from 'react'
 import  ReactDom  from 'react-dom'
 import './Modal.css'
+import ClearIcon from '@mui/icons-material/Clear';
+import ResponsiveProvider from '../../../Context/ResponsiveContext';
 
 export default function Modal({open, onClose, children}) {
+    const responsive_ctx = useContext(ResponsiveProvider);
+    const width = responsive_ctx.screenWidth;
+    console.log(width)
     if (!open) return null
-  return ReactDom.createPortal(
+    return  ReactDom.createPortal(
     <>
     <div className='modal'>
         <div className='modal-content'>
             {children}
-            <Button animation='none' text='X' style={{position: 'absolute', fontSize: '1.3rem', border: 'none', borderRadius: '10px', backgroundColor: 'transparent', color: 'white', top: '2%', right: '2%'}} function={onClose}/>
+            <Button animation='none' text={<ClearIcon/>} style={{position: 'absolute', fontSize: '1.3rem', border: 'none', borderRadius: '10px', backgroundColor: 'transparent', color: 'black', top: '1%', right: '1%'}} function={onClose}/>
         </div>
     </div>
     </>,
