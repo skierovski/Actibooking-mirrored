106f4ececbe2606330479b1958919adce7a7f3d3
Patryk Skierkowski <salon.skierkowski@gmail.com>
2023-02-22 15:36:59 +0100

    working on modal

--- actibooking_react_app/actibooking-newui/src/Components/Default/Forms/LogInForm/LogInForm.js
+++ actibooking_react_app/actibooking-newui/src/Components/Default/Forms/LogInForm/LogInForm.js
@@ -7,7 +7,7 @@ const LogInForm = () => {
     return ( 
         <>
     <FormControl required={true}>
-        <TextField type='email' sx={{width: {sm: 500}}} id="outlined-basic" label="Email" variant="outlined" margin="dense" size="small" />
+        <TextField type='email' sx={{width: {sm: 700, xs: 385}}} id="outlined-basic" label="Email" variant="outlined" margin="dense" size="small" />
         <TextField type='password' id="outlined-basic" sx={{ color: 'red' }} label="Password" variant="outlined" margin="dense" size="small" />
     </FormControl>
     <Typography sx={{fontSize: '0.7rem'}} variant='subtitle1' noWrap={true}>You dont have account ? <Button style={{fontSize: '0.7rem', display: 'inline-block', position: 'relative', border: 'none', background: 'transparent'}} functionProps={true}  text='register here!' animation='change-color-text'/>
